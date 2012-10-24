c4ea96987e9bfb92c3df00b2ca4d82fabfdcdd4c
Raissa Mustapa <20744555@ecm-csse2520-l.uniwa.uwa.edu.au>
2012-10-24 18:50:41 +0800

    Working with certificates

--- Client.java
+++ Client.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.net.UnknownHostException;
 import java.security.InvalidKeyException;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
@@ -11,39 +12,92 @@ import javax.net.ssl.*;
 
 
 public class Client {
+	static private java.security.cert.Certificate cert;
+	static private OutputStream out;
+	static private DataOutputStream outData;
+	static private SSLSocket cSock;
+	
+	static public SSLSocket newSocket(String hostname, int port) throws UnknownHostException, IOException, InvalidKeyException, CertificateException, NoSuchAlgorithmException, NoSuchProviderException, SignatureException {
+		SSLSocketFactory factory = HttpsURLConnection.getDefaultSSLSocketFactory();
+		SSLSocket sock = (SSLSocket)factory.createSocket(hostname, port);
+		sock.startHandshake();
+		java.security.cert.Certificate[] serverCerts =
+	    	sock.getSession().getPeerCertificates();
+	    
+	    // Let's do some stuff yo
+	    System.out.println("Look we have the server's certificates!");
+	    // Verify
+	    serverCerts[0].verify(cert.getPublicKey());
+	    System.out.println("And it's verified!");
+		return sock;
+	}
+	
+	static public void closeAll() throws IOException {
+		cSock.close();
+		out.close();
+		outData.close();
+	}
+	
 	static public void main(String argv[]) {
 		try {
+			if(argv.length < 2) {
+				System.out.println("Usage: Client [port] [filenames]");
+				System.exit(0);
+			}
 		    // Create the client socket
 		    int port = Integer.parseInt(argv[0]);
 		    String hostname = "localhost";
 		    System.setProperty("javax.net.ssl.trustStore", "Client/cTrustStore");
-			System.setProperty("javax.net.ssl.trustStorePassword", "keystorePW");
-			// Get server certificate
+			System.setProperty("javax.net.ssl.trustStorePassword", "ckeystorePW");
+			// Get server certificate from truststore
 		  	FileInputStream is = new FileInputStream(System.getProperty("javax.net.ssl.trustStore"));
 
 		    KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType());
-		    keystore.load(is, "keystorePW".toCharArray());
+		    keystore.load(is, "ckeystorePW".toCharArray());
 
 		    // Get certificate
-		    java.security.cert.Certificate cert = keystore.getCertificate("server");
+		    cert = keystore.getCertificate("server");
 		    
-		    SSLSocketFactory factory = HttpsURLConnection.getDefaultSSLSocketFactory();
-		    SSLSocket socket = (SSLSocket)factory.createSocket(hostname, port);
-		    System.out.println("Handshake...");
-		    // Connect to the server
-		    socket.startHandshake();
-		    System.out.println("Retrieve certs");
-		    // Retrieve the server's certificate chain
-		    java.security.cert.Certificate[] serverCerts =
-		        socket.getSession().getPeerCertificates();
-		    
-		    // Let's do some stuff yo
-		    System.out.println("Look we have the server's certificates!");
-		    // Verify
-		    serverCerts[0].verify(cert.getPublicKey());
-		    System.out.println("And it's verified!");
-		    // Close the socket
-		    socket.close();
+		    // BEGIN ACTUAL CLIENT STUFF
+		    String fname;
+		    InputStream in;
+		    File f;
+		    long flen;
+		    // Send files
+		    for(int i = 1; i<argv.length; i++) {
+			    System.out.println("Start file loop");
+			    cSock = newSocket(hostname, port);
+			    System.out.println("Created socket");
+			    out = cSock.getOutputStream(); // Get the output stream
+			    System.out.println("Got Output Stream");
+			    outData = new DataOutputStream(out); // To send bytes as strings'
+			    System.out.println("Got data output stream");
+			    fname = argv[i];
+			    System.out.println("Got name"+fname);
+			    in = new FileInputStream(fname);
+			    System.out.println("File input stream");
+			    f = new File(fname);
+			    System.out.println("Open file");
+			    flen = f.length();
+			    System.out.println("Got length");
+			    fname = f.getName();
+			    System.out.println("Sent name "+fname);
+			    outData.writeBytes("\r\nSTARTFILEREADING\r\n"+fname+"\r\n"+flen+"\r\n");
+			    System.out.println("Sent name "+fname);
+			    closeAll();
+			    cSock = newSocket(hostname, port); // Send file
+			    out = cSock.getOutputStream(); // Get the output stream
+			    byte[] buf = new byte[1024];
+			    int len;
+			    int total = 0;
+			    while((len = in.read(buf)) > 0) {
+				    out.write(buf, 0, len);
+				    total += len;
+			    }
+			    System.out.println("Total "+total+" bytes sent.");
+			    closeAll();
+			    in.close();
+		    }
 		} catch (SSLPeerUnverifiedException e) {
 			System.out.println(e.getMessage());
 			System.exit(0);
--- Server.java
+++ Server.java
@@ -9,25 +9,69 @@ import javax.net.ssl.SSLServerSocketFactory;
 public class Server {
 	public static void main(String argv[]) {
 		try {
+			if(argv.length < 1) {
+				System.out.println("Usage: Server [port]");
+				System.exit(0);
+			}
 			int port = Integer.parseInt(argv[0]);
+			String folder;
+			if(argv.length > 1)
+				folder = argv[1]+"/";
+			else
+				folder = "";
 			System.setProperty("javax.net.ssl.keyStore", "Server/keystore.jks");
 			System.setProperty("javax.net.ssl.keyStorePassword", "keystorePW");
-			
-			System.out.println("Server setting up port "+port+", press 'ENTER' to close connection...");
+
+			System.out.println("Server setting up port "+port);
 			ServerSocketFactory ssocketFactory = SSLServerSocketFactory.getDefault();
 		    ServerSocket servSock = ssocketFactory.createServerSocket(port);
-		    
-			while(true) {
-				Socket sock = servSock.accept(); // Listening socket 
-				System.out.println("Receiving something!");
-				// Receive name from client
-				InputStream fileIn = sock.getInputStream();
-				BufferedReader in = new BufferedReader(new InputStreamReader(fileIn));
-				String s = in.readLine();
-				System.out.println(s);
-			}
+		    String fname;
+		    long flen;
+		    OutputStream out;
+		    File f;
+		    byte[] buffer = new byte[1024];
+		    int len;
+		    boolean end = false;
+		    while(true) {
+			    Socket sock = servSock.accept(); // Listening socket
+			    System.out.println("Receiving something!");
+			    // Receive name from client
+			    InputStream fileIn = sock.getInputStream();
+			    BufferedReader in = new BufferedReader(new InputStreamReader(fileIn));
+			    while((fname = in.readLine()) != null && !fname.equals("STARTFILEREADING")) {}
+			    System.out.println("Line break: "+fname);
+			    fname = in.readLine();
+			    flen = Long.parseLong(in.readLine());
+			    sock = servSock.accept();
+			    fileIn = sock.getInputStream();
+			    f = new File(folder+fname); // Open file stream with specified name
+			    System.out.println("Created file stream for file "+fname+" with "+flen+" bytes");
+			    out = new FileOutputStream(f);
+			    while(!end && flen > 1024) {
+				    if((len = fileIn.read(buffer)) != -1) {
+				    	out.write(buffer, 0, len);
+				    	flen -= len;
+				    }
+				    else
+				    	end = true;
+			    }
+			    System.out.println("Left to read: "+flen+" bytes");
+			    if(end || (len = fileIn.read(buffer, 0, (int) flen)) == -1)
+			    	System.out.println("File end reached unexpectedly");
+			    else {
+			    	while(flen > 0 && len != -1) {
+			    		out.write(buffer, 0, len);
+			    		flen -= len;
+			    		if(flen > 0)
+			    			len = fileIn.read(buffer, 0, (int) flen);
+			    	}
+			    }
+			    out.close();
+			    System.out.println("File written!");
+			    System.out.println("END");
+		    }
 		}
-		catch(IOException e) {
+		catch(Exception e) {
 			System.out.print(e.getMessage());
 		}
 	}
