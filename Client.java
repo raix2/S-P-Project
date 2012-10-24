694c4e5dcc0f385f536017b3072302b33775f5fb
Raissa Mustapa <20744555@ecm-csse2520-l.uniwa.uwa.edu.au>
2012-10-24 21:22:29 +0800

    Ready to take commands

--- Client.java
+++ Client.java
@@ -16,8 +16,11 @@ public class Client {
 	static private OutputStream out;
 	static private DataOutputStream outData;
 	static private SSLSocket cSock;
+	static private String hostname;
+	static private int port;
+	static private InputStream in;
 	
-	static public SSLSocket newSocket(String hostname, int port) throws UnknownHostException, IOException, InvalidKeyException, CertificateException, NoSuchAlgorithmException, NoSuchProviderException, SignatureException {
+	static public SSLSocket newSocket() throws UnknownHostException, IOException, InvalidKeyException, CertificateException, NoSuchAlgorithmException, NoSuchProviderException, SignatureException {
 		SSLSocketFactory factory = HttpsURLConnection.getDefaultSSLSocketFactory();
 		SSLSocket sock = (SSLSocket)factory.createSocket(hostname, port);
 		sock.startHandshake();
@@ -38,6 +41,55 @@ public class Client {
 		outData.close();
 	}
 	
+	static public void upload(String[] argv) throws IOException, InvalidKeyException, CertificateException, NoSuchAlgorithmException, NoSuchProviderException, SignatureException {
+	    String fname;
+	    InputStream in;
+	    File f;
+	    long flen;
+	    // Send files
+	    for(int i = 1; i<argv.length; i++) {
+		    System.out.println("Start file loop");
+		    cSock = newSocket();
+		    System.out.println("Created socket");
+		    out = cSock.getOutputStream(); // Get the output stream
+		    System.out.println("Got Output Stream");
+		    outData = new DataOutputStream(out); // To send bytes as strings
+		    System.out.println("Got data output stream");
+		    fname = argv[i];
+		    System.out.println("Got name"+fname);
+		    in = new FileInputStream(fname);
+		    System.out.println("File input stream");
+		    f = new File(fname);
+		    System.out.println("Open file");
+		    flen = f.length();
+		    System.out.println("Got length");
+		    fname = f.getName();
+		    System.out.println("Sent name "+fname);
+		    outData.writeBytes("\r\nSTARTFILEREADING\r\n"+fname+"\r\n"+flen+"\r\n");
+		    System.out.println("Sent name "+fname);
+		    closeAll();
+		    cSock = newSocket(); // Send file
+		    out = cSock.getOutputStream(); // Get the output stream
+		    byte[] buf = new byte[1024];
+		    int len;
+		    int total = 0;
+		    while((len = in.read(buf)) > 0) {
+			    out.write(buf, 0, len);
+			    total += len;
+		    }
+		    System.out.println("Total "+total+" bytes sent.");
+		    closeAll();
+		    in.close();
+	    }
+	    cSock = newSocket();
+	    System.out.println("Ending socket");
+	    out = cSock.getOutputStream(); // Get the output stream
+	    System.out.println("Got Output Stream");
+	    outData = new DataOutputStream(out); // To send bytes as strings
+	    outData.writeBytes("\n\rENDFILEUPLOAD\n\r");
+	    closeAll();
+	}
+	
 	static public void main(String argv[]) {
 		try {
 			if(argv.length < 2) {
@@ -45,8 +97,8 @@ public class Client {
 				System.exit(0);
 			}
 		    // Create the client socket
-		    int port = Integer.parseInt(argv[0]);
-		    String hostname = "localhost";
+		    port = Integer.parseInt(argv[0]);
+		    hostname = "localhost";
 		    System.setProperty("javax.net.ssl.trustStore", "Client/cTrustStore");
 			System.setProperty("javax.net.ssl.trustStorePassword", "ckeystorePW");
 			// Get server certificate from truststore
@@ -59,67 +111,39 @@ public class Client {
 		    cert = keystore.getCertificate("server");
 		    
 		    // BEGIN ACTUAL CLIENT STUFF
-		    String fname;
-		    InputStream in;
-		    File f;
-		    long flen;
-		    // Send files
-		    for(int i = 1; i<argv.length; i++) {
-			    System.out.println("Start file loop");
-			    cSock = newSocket(hostname, port);
-			    System.out.println("Created socket");
-			    out = cSock.getOutputStream(); // Get the output stream
-			    System.out.println("Got Output Stream");
-			    outData = new DataOutputStream(out); // To send bytes as strings'
-			    System.out.println("Got data output stream");
-			    fname = argv[i];
-			    System.out.println("Got name"+fname);
-			    in = new FileInputStream(fname);
-			    System.out.println("File input stream");
-			    f = new File(fname);
-			    System.out.println("Open file");
-			    flen = f.length();
-			    System.out.println("Got length");
-			    fname = f.getName();
-			    System.out.println("Sent name "+fname);
-			    outData.writeBytes("\r\nSTARTFILEREADING\r\n"+fname+"\r\n"+flen+"\r\n");
-			    System.out.println("Sent name "+fname);
-			    closeAll();
-			    cSock = newSocket(hostname, port); // Send file
-			    out = cSock.getOutputStream(); // Get the output stream
-			    byte[] buf = new byte[1024];
-			    int len;
-			    int total = 0;
-			    while((len = in.read(buf)) > 0) {
-				    out.write(buf, 0, len);
-				    total += len;
-			    }
-			    System.out.println("Total "+total+" bytes sent.");
-			    closeAll();
-			    in.close();
+		    // Let's receive a welcome message first
+		    // Open first socket
+		    cSock = newSocket();
+		    in = cSock.getInputStream();
+		    BufferedReader br = new BufferedReader(new InputStreamReader(in));
+		    String s;
+		    if(!(s = br.readLine()).startsWith("Welcome")) {
+		    	System.err.println("Error: Server welcome not received.");
+		    	System.exit(0);
 		    }
+		    System.out.println(s);
 		} catch (SSLPeerUnverifiedException e) {
 			System.out.println(e.getMessage());
 			System.exit(0);
 		} catch (IOException e) {
 			System.out.println(e.getMessage());
 			System.exit(0);
-		} catch (InvalidKeyException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		} catch (CertificateException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (NoSuchAlgorithmException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} catch (KeyStoreException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		} catch (NoSuchProviderException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (SignatureException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (KeyStoreException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
--- Server.java
+++ Server.java
@@ -5,8 +5,69 @@ import java.net.Socket;
 import javax.net.ServerSocketFactory;
 import javax.net.ssl.SSLServerSocketFactory;
 
-
 public class Server {
+	static private ServerSocketFactory ssocketFactory;
+	static private ServerSocket servSock;
+	static private OutputStream out;
+	static private DataOutputStream dataOut;
+	static private InputStream in;
+	static private BufferedReader br;
+	static private Socket sock;
+	
+	public static void upload(String[] argv) throws IOException {
+		String folder;
+		if(argv.length > 1)
+			folder = argv[1]+"/";
+		else
+			folder = "";
+	    String fname;
+	    long flen;
+	    File f;
+	    byte[] buffer = new byte[1024];
+	    int len;
+	    boolean end = false;
+	    while(true) {
+		    sock = servSock.accept(); // Listening socket
+		    System.out.println("Receiving something!");
+		    in = sock.getInputStream();
+		    br = new BufferedReader(new InputStreamReader(in));
+		    while((fname = br.readLine()) != null && !fname.equals("STARTFILEREADING") && !fname.equals("ENDFILEUPLOAD")) {}
+		    System.out.println("Line break: "+fname);
+		    if(fname.equals("ENDFILEUPLOAD") || fname == null)
+		    	break;
+		    fname = br.readLine();
+		    flen = Long.parseLong(br.readLine());
+		    sock = servSock.accept();
+		    in = sock.getInputStream();
+		    f = new File(folder+fname); // Open file stream with specified name
+		    System.out.println("Created file stream for file "+fname+" with "+flen+" bytes");
+		    out = new FileOutputStream(f);
+		    while(!end && flen > 1024) {
+			    if((len = in.read(buffer)) != -1) {
+			    	out.write(buffer, 0, len);
+			    	flen -= len;
+			    }
+			    else
+			    	end = true;
+		    }
+		    System.out.println("Left to read: "+flen+" bytes");
+		    if(end || (len = in.read(buffer, 0, (int) flen)) == -1)
+		    	System.out.println("File end reached unexpectedly");
+		    else {
+		    	while(flen > 0 && len != -1) {
+		    		out.write(buffer, 0, len);
+		    		flen -= len;
+		    		if(flen > 0)
+		    			len = in.read(buffer, 0, (int) flen);
+		    	}
+		    }
+		    out.close();
+		    System.out.println("File written!");
+		    System.out.println("END");
+	    }
+	    System.out.println("Transfer ended.");
+	}
+	
 	public static void main(String argv[]) {
 		try {
 			if(argv.length < 1) {
@@ -14,62 +75,21 @@ public class Server {
 				System.exit(0);
 			}
 			int port = Integer.parseInt(argv[0]);
-			String folder;
-			if(argv.length > 1)
-				folder = argv[1]+"/";
-			else
-				folder = "";
 			System.setProperty("javax.net.ssl.keyStore", "Server/keystore.jks");
 			System.setProperty("javax.net.ssl.keyStorePassword", "keystorePW");
 
 			System.out.println("Server setting up port "+port);
-			ServerSocketFactory ssocketFactory = SSLServerSocketFactory.getDefault();
-		    ServerSocket servSock = ssocketFactory.createServerSocket(port);
-		    String fname;
-		    long flen;
-		    OutputStream out;
-		    File f;
-		    byte[] buffer = new byte[1024];
-		    int len;
-		    boolean end = false;
-		    while(true) {
-			    Socket sock = servSock.accept(); // Listening socket
-			    System.out.println("Receiving something!");
-			    // Receive name from client
-			    InputStream fileIn = sock.getInputStream();
-			    BufferedReader in = new BufferedReader(new InputStreamReader(fileIn));
-			    while((fname = in.readLine()) != null && !fname.equals("STARTFILEREADING")) {}
-			    System.out.println("Line break: "+fname);
-			    fname = in.readLine();
-			    flen = Long.parseLong(in.readLine());
-			    sock = servSock.accept();
-			    fileIn = sock.getInputStream();
-			    f = new File(folder+fname); // Open file stream with specified name
-			    System.out.println("Created file stream for file "+fname+" with "+flen+" bytes");
-			    out = new FileOutputStream(f);
-			    while(!end && flen > 1024) {
-				    if((len = fileIn.read(buffer)) != -1) {
-				    	out.write(buffer, 0, len);
-				    	flen -= len;
-				    }
-				    else
-				    	end = true;
-			    }
-			    System.out.println("Left to read: "+flen+" bytes");
-			    if(end || (len = fileIn.read(buffer, 0, (int) flen)) == -1)
-			    	System.out.println("File end reached unexpectedly");
-			    else {
-			    	while(flen > 0 && len != -1) {
-			    		out.write(buffer, 0, len);
-			    		flen -= len;
-			    		if(flen > 0)
-			    			len = fileIn.read(buffer, 0, (int) flen);
-			    	}
-			    }
-			    out.close();
-			    System.out.println("File written!");
-			    System.out.println("END");
-		    }
+			ssocketFactory = SSLServerSocketFactory.getDefault();
+			servSock = ssocketFactory.createServerSocket(port);
+			while(true) {
+				//Aaaaand we have connection!
+				sock = servSock.accept();
+				System.out.println("A user has connected!");
+				out = sock.getOutputStream();
+				dataOut = new DataOutputStream(out);
+				dataOut.writeBytes("Welcome to the Cloud\n");
+				System.out.println("Sent welcome =D");
+			}
 		}
 		catch(Exception e) {
 			System.out.print(e.getMessage());
